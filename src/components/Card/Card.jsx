9ab7b964246a4cec068d8361a8120de92949ec56
Margarita Poole <margaritapoole311@gmail.com>
2022-04-26 14:37:46 -0500

    Added card data. Created card tiles.

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,5 +1,27 @@
-const Card = () => {
-  return <div>travel card goes here</div>
+import pin from '../../assets/mapIcon.png'
+
+const Card = ({ item }) => {
+  return (
+    <div className='border-slate-900 border-2'>
+      <div>
+        <img src={item.image} alt='cardImage' />
+        <div>
+          <div>
+            <img src={pin} alt='map pin' />
+            <span>{item.location}</span>
+          </div>
+          <span>View on Google Maps link</span>
+        </div>
+        <p>{item.title}</p>
+        <div>
+          <p>
+            {item.startDate} - {item.endDate}
+          </p>
+          <p>{item.description}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Card
--- src/components/Card/data.js
+++ src/components/Card/data.js
@@ -4,6 +4,7 @@ import Geirangerfjord from '../../assets/Geirangerfjord.png'
 
 const cardData = [
   {
+    id: 1,
     title: 'Mount Fuji',
     location: 'Japan',
     googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
@@ -11,9 +12,10 @@ const cardData = [
     endDate: '24 Jan, 2021',
     description:
       'Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.',
-    image: { MountFuji },
+    image: MountFuji,
   },
   {
+    id: 2,
     title: 'Sydney Opera House',
     location: 'Australia',
     googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
@@ -21,9 +23,10 @@ const cardData = [
     endDate: '08 Jun, 2021',
     description:
       "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings",
-    image: { SydneyOperaHouse },
+    image: SydneyOperaHouse,
   },
   {
+    id: 3,
     title: 'Geirangerfjord',
     location: 'Norway',
     googleMapsUrl: 'https://goo.gl/maps/1DGM5WrWnATgkSNB8',
@@ -31,7 +34,7 @@ const cardData = [
     endDate: '18 Nov, 2021',
     description:
       'The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.',
-    image: { Geirangerfjord },
+    image: Geirangerfjord,
   },
 ]
 
